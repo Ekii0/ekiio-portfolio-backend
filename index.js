cab34fd844a14f5530444d55f650b1e820d3be97
Ekii0 <ekiio@protonmail.com>
2023-11-23 22:43:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+dotenv.config();
+app.use(express.json());
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.MAIL_USERNAME,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_KEY,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
+
+app.post("/sendmail", (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  let mailOptions = {
+    sender: email,
+    replyTo: email,
+    to: process.env.TARGET_EMAIL_ADDRESS,
+    subject: subject,
+    text: `Hi, I am ${name} (${email}).\n ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+      res.status(500).send("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}...`);
+});
